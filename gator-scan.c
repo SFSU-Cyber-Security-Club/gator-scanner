403147dbdffa1a4182815383fc5958aa78dc240e
devilmanCr0 <101080560+devilmanCr0@users.noreply.github.com>
2023-11-26 02:55:06 -0800

    need to cook

---
+++ gator-scan.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>    // inet_addr
+#include <sys/socket.h>   // packet manipulation
+#include <netinet/ip.h>   // ip header declarations
+#include <netinet/tcp.h>  // tcp header declarations
+
+
+
